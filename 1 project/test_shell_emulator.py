20b333267e298ea0d5224e4e49c0914807c0366c
Dobryak-Dontow <S9057959911@yandex.ru>
2024-11-11 21:37:56 +0300

    test_shell_emulator.py

--- 1 project/test_shell_emulator.py
+++ 1 project/test_shell_emulator.py
@@ -8,7 +8,7 @@ class TestShellEmulator(unittest.TestCase):
 
     def setUp(self):
         self.config = {
-            "fs_path": "test_virtual_filesystem.tar",
+            "fs_path": "virtual_filesystem.tar",
             "log_path": "test_log.xml",
             "start_script_path": "start_script.sh"
         }
@@ -19,33 +19,28 @@ class TestShellEmulator(unittest.TestCase):
 
         # Создание тестового архива
         self.emulator = ShellEmulator('test_config.json')
-        self.emulator.create_virtual_fs()  # Создаем виртуальную файловую систему
 
     def tearDown(self):
         # Удаляем созданные файлы после тестов
-        if os.path.exists(self.config['fs_path']):
-            os.remove(self.config['fs_path'])
+        #if os.path.exists(self.config['fs_path']):
+        #    os.remove(self.config['fs_path'])
         if os.path.exists(self.config['log_path']):
             os.remove(self.config['log_path'])
         if os.path.exists('test_config.json'):
             os.remove('test_config.json')
 
     def test_load_virtual_fs(self):
-        self.emulator.load_virtual_fs()
-        # Проверяем, что директория была создана
-        self.assertTrue(os.path.exists('virtual_fs'))
-        self.assertTrue(os.path.isdir('virtual_fs'))
+        self.assertTrue(len(self.emulator.fs_structure) != 0)
 
     def test_ls_command(self):
-        self.emulator.load_virtual_fs()
+        self.emulator.execute_command('cd virtual_filesystem')
         result = self.emulator.execute_command('ls')
-        expected_files = {'file1.txt', 'file2.txt', 'directory'}
+        expected_files = {'file1.txt', 'directory'}
         self.assertTrue(expected_files.issubset(set(result.split('\n'))))
 
     def test_cd_command(self):
-        self.emulator.load_virtual_fs()
-        self.emulator.execute_command('cd directory')
-        self.assertEqual(self.emulator.current_directory, os.path.join('virtual_fs', 'directory'))
+        self.emulator.execute_command('cd virtual_filesystem')
+        self.assertEqual(self.emulator.current_directory, "virtual_filesystem")
 
         # Проверка на переход в несуществующую директорию
         result = self.emulator.execute_command('cd non_existing_directory')
@@ -56,8 +51,9 @@ class TestShellEmulator(unittest.TestCase):
         self.assertEqual(result, "Exiting...")
 
     def test_cp_command_not_implemented(self):
+        self.emulator.execute_command('cd virtual_filesystem')
         result = self.emulator.execute_command('cp file1.txt file2.txt')
-        self.assertEqual(result, "Copied from file1.txt to file2.txt (not implemented)")
+        self.assertEqual(result, "Copied from file1.txt to file2.txt")
 
 if __name__ == '__main__':
     unittest.main()
